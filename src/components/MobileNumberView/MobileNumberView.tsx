9221a99ebbfc2da28334a4ce20f03491a1d9cee5
VanyaMate <buhu653@mail.ru>
2023-10-24 16:58:46 +0300

    start components

---
+++ src/components/MobileNumberView/MobileNumberView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const MobileNumberView = () => {
+    return (
+        <div>
+            MobileNumberView
+        </div>
+    );
+};
+
+export default MobileNumberView;
