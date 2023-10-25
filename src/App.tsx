8507ab45805e5f2cb61f89c8b01ebf88718a6179
VanyaMate <buhu653@mail.ru>
2023-10-25 19:38:57 +0300

    добавил всё в microwindow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import KeyboardMobileNumberForm
     from '@/containers/KeyboardMobileNumberForm/KeyboardMobileNumberForm.tsx';
+import MicroWindow from '@/layouts/MicroWindow/MicroWindow.tsx';
 
 
 const App = () => {
     return (
-        <div>
-            <KeyboardMobileNumberForm/>
+        <div style={ {
+            height        : '100vh',
+            display       : 'flex',
+            alignItems    : 'center',
+            justifyContent: 'center',
+        } }>
+            <div style={ {
+                border      : '2px dotted rgba(0, 0, 0, .2)',
+                borderRadius: '10px',
+                padding     : 20,
+            } }>
+                <MicroWindow>
+                    <KeyboardMobileNumberForm/>
+                </MicroWindow>
+            </div>
         </div>
     );
 };
